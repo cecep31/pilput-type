394560d2c2c467cc7ee3f0ce20d66a1a53b52cb1
cecep31 <cecepjanuardi31@gmail.com>
2023-07-25 23:24:35 +0700

    update env

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,9 @@ export default defineConfig({
       API_URL: "https://v2.pilput.dev",
       WS_URL: "wss://v2.pilput.dev",
       STORAGE_URL: "https://d42zd71vraxqs.cloudfront.net/",
-      DOMAIN: "pilput.test",
-      MAIN_URL: "http://pilput.test",
-      DASH_URL: "http://dash.pilput.test",
+      DOMAIN: "pilput.dev",
+      MAIN_URL: "https://pilput.dev",
+      DASH_URL: "https://dash.pilput.dev",
     },
   },
   resolve: {
