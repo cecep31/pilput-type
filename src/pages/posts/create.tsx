3763972f4e58e2a0879e69b90733ac6a97356618
cecep31 <cecepjanuardi31@gmail.com>
2024-01-28 09:51:54 +0700

    fix baseurl storage

--- src/pages/posts/create.tsx
+++ src/pages/posts/create.tsx
@@ -9,6 +9,7 @@ import toast from "react-hot-toast";
 import { convertToSlug } from "@/utils/slug";
 import axios, { AxiosError } from "axios";
 import { useState } from "react";
+import {storagebaseurl} from '@/utils/getConfig'
 
 interface MyResponse {
   error?: {
@@ -108,7 +109,7 @@ function PostCreate() {
           </div>
 
           <div className="flex justify-center">
-            <img src={process.env.VITE_STORAGE_URL+""+post.photo_url} alt="" className="max-h-96" />
+            <img src={storagebaseurl+""+post.photo_url} alt="" className="max-h-96" />
           </div>
           <div className="my-7 w-full">
             <input
---
+++ src/utils/getConfig.ts
@@ -0,0 +1,6 @@
+export const apibaseurl = import.meta.env.VITE_API_URL || "";
+export const dashbaseurl = import.meta.env.VITE_DASH_URL || "";
+export const wsbaseurl = import.meta.env.VITE_WS_URL || "";
+export const storagebaseurl = import.meta.env.VITE_STORAGE_URL || "";
+export const mainbaseurl = import.meta.env.VITE_MAIN_URL || "";
+export const domain = import.meta.env.VITE_DOMAIN || "";
