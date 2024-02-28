beab77475c0261e72fabe66c75e1462b0793c947
cecep31 <cecepjanuardi31@gmail.com>
2024-02-28 15:58:04 +0700

    update logrocket

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,10 @@ import {Toaster} from 'react-hot-toast'
 import "./global.css";
 import { router } from "./routes/router";
 import { RouterProvider } from "react-router-dom";
+import LogRocket from 'logrocket';
 
 
+LogRocket.init('lcw09w/pilput');
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
